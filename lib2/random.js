999a8e814f673b61df5b66bdbbb38c8613fe1cbf
wuyanfei <wuyanfei58@gmail.com>
2012-12-07 21:04:15 +0800

    .

--- lib2/random.js
+++
@@ -1,8 +0,0 @@
-var random=function(url){
-	var numInput = new Number(10000);
-	var numOutput = new Number(Math.random() * numInput).toFixed(0);
-	url = url+numOutput;
-	return url;
-}
-exports.random=random;
-
